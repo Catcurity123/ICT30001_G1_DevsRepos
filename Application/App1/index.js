4a8bde6caa5ebf42078187e465867f1a366d73fc
C57c4r1t1 <dangviluanwk@gmail.com>
2024-06-21 08:55:16 +0700

    Check

--- Application/App1/index.js
+++ Application/App1/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From teamadmin1" }');
+    res.send('{ "response": "Hello From teamadmin1_3" }');
 });
 
 app.get('/will', function (req, res) {
