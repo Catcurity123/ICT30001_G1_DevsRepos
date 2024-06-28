9c252c7815e0c54cdce14793f1d114c04abbbeea
C57c4r1t1 <dangviluanwk@gmail.com>
2024-06-29 04:34:11 +0700

    Fix

--- Application/App3/Docker/src/server.js
+++ Application/App3/Docker/src/server.js
@@ -9,27 +9,32 @@ app.use(function(req, res, next) {
 
 // Hard-Coded for demo
 const CONTACTS = [
-                    {
-                      "name": "Foo Bar",
-                      "email": "foobar@test.com",
-                      "cell": "555-123-4567"
-                    },
-                    {
-                      "name": "Biz Baz",
-                      "email": "bizbaz@test.com",
-                      "cell": "555-123-5678"
-                    },
-                    {
-                      "name": "Bing Bang",
-                      "email": "bingbang@test.com",
-                      "cell": "555-123-6789"
-                    }
-                ];
+    {
+        "name": "Foo Bar",
+        "email": "foobar@test.com",
+        "cell": "555-123-4567"
+    },
+    {
+        "name": "Biz Baz",
+        "email": "bizbaz@test.com",
+        "cell": "555-123-5678"
+    },
+    {
+        "name": "Bing Bang",
+        "email": "bingbang@test.com",
+        "cell": "555-123-6789"
+    }
+];
 
-app.get('/contacts', (req, res) => {
-  res.json({contacts: CONTACTS});
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Contacts API!');
 });
 
-app.listen(3000, () =>{
-   console.log('Server running on port 3000.'); 
+app.get('/contacts', (req, res) => {
+    res.json({ contacts: CONTACTS });
+});
+
+app.listen(80, () => {
+    console.log('Server running on port 80.');
 });
