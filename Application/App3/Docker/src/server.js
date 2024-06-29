f304056838a4102faa1e56613793e5f95796d22b
C57c4r1t1 <dangviluanwk@gmail.com>
2024-06-29 07:06:45 +0700

    Check: check syncing to Argo

--- Application/App3/Docker/src/server.js
+++ Application/App3/Docker/src/server.js
@@ -28,7 +28,7 @@ const CONTACTS = [
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Contacts API!');
+    res.send('Welcome to the Contacts API! Checking Sync');
 });
 
 app.get('/contacts', (req, res) => {
