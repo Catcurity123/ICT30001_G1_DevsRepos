6a88cdd7780acc9cb3f3b62a33a2730d2e64f4b5
C57c4r1t1 <dangviluanwk@gmail.com>
2024-08-02 12:48:07 +0700

    Check

--- Application/Mexer/app/_lib/constants.tsx
+++ Application/Mexer/app/_lib/constants.tsx
@@ -1,5 +1,5 @@
 export class Constants {
-   public static readonly ENDPOINT: string = "http://localhost:3000";
-  //public static readonly ENDPOINT: string = "http://mexerapi.heyyytamvo.io.vn"
+   // public static readonly ENDPOINT: string = "http://localhost:3000";
+  public static readonly ENDPOINT: string = "http://mexerapi.heyyytamvo.io.vn"
   // public static readonly USERS: string = `${ApiEndpoints.BASE_URL}/users`;
 }
